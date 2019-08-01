0037bcd970d0adc2a050d3e1e8ee25833c833d3c
Eduardo Rubio <erubioteruel@redventures.com>
2019-08-01 17:47:32 -0400

    Remodeled the layout and built the GitHub tile and tied its API in

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './style.css';
 import Navbar from './Components/Navbar.jsx';
 import Tile from './Components/Tile.jsx';
 import { Container } from 'react-bootstrap';
-import TestJob from "./Utils/testJob"
+import TestJob from "./Utils/GitHubPullReqs"
 import GitHubTile from './Components/ChildTiles/GitHubTile'
 import CircleCITile from './Components/ChildTiles/CircleCITile'
 require('dotenv').config();
@@ -16,50 +16,37 @@ function App() {
         <div className="container-fluid">
          <div className="card-columns">
           {/* GitHub List of Pull Requests */}
-          <div class="card">
-            <h4 className="card-header">
-              Open Pull Requests
-            </h4>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Allconnext <span className="float-right">4</span></li>
-              <li class="list-group-item">Allconnect.com <span className="float-right">9</span></li>
-              <li class="list-group-item">Allconnext-WP <span className="float-right">11</span></li>
-              <li class="list-group-item">Allconnect-Blog <span className="float-right">0</span></li>
-            </ul>
-            <div className="card-footer">
-            <a href="#" class="card-link">Card link</a>
-            </div>
-          </div>
+          <GitHubTile repos={["allconnext", "allconnext-wp", "allconnect.com-blog", "allconnect.com"]}/>
 
           {/* CCI Repo Statuses */}
-          <div class="card">
+          <div className="card">
             <h4 className="card-header">
               CircelCI Repo Statuses
             </h4>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Allconnext <span className="float-right">Success</span></li>
-              <li class="list-group-item">Allconnect.com <span className="float-right">Success</span></li>
-              <li class="list-group-item">Allconnext-WP <span className="float-right">Failure</span></li>
-              <li class="list-group-item">Allconnext <span className="float-right">Success</span></li>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">Allconnext <span className="float-right">Success</span></li>
+              <li className="list-group-item">Allconnect.com <span className="float-right">Success</span></li>
+              <li className="list-group-item">Allconnext-WP <span className="float-right">Failure</span></li>
+              <li className="list-group-item">Allconnext <span className="float-right">Success</span></li>
             </ul>
             <div className="card-footer">
-            <a href="#" class="card-link">Card link</a>
+            <a href="#" className="card-link">Card link</a>
             </div>
           </div>
 
           {/* Open Multidev Environments  */}
-          <div class="card">
+          <div className="card">
             <h4 className="card-header">
               Open Multidev Environments 
             </h4>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Allconnext <span className="float-right">6</span></li>
-              <li class="list-group-item">Allconnect Connected Home Blog <span className="float-right">3</span></li>
-              <li class="list-group-item">Allconnect-WP <span className="float-right">3</span></li>
-              <li class="list-group-item">Move Connection Center <span className="float-right">2</span></li>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">Allconnext <span className="float-right">6</span></li>
+              <li className="list-group-item">Allconnect Connected Home Blog <span className="float-right">3</span></li>
+              <li className="list-group-item">Allconnect-WP <span className="float-right">3</span></li>
+              <li className="list-group-item">Move Connection Center <span className="float-right">2</span></li>
             </ul>
               <div className="card-footer">
-                <a href="#" class="card-link">Card link</a>
+                <a href="#" className="card-link">Card link</a>
               </div>          
             </div>
         </div>
@@ -71,7 +58,7 @@ function App() {
          <div className="card-columns">
           {/* GitHub List of Pull Requests */}
           {/* Visitor Count  */}
-          <div class="card">
+          <div className="card">
             <h4 className="card-header"> 
               Allconnext Page Load Speed
             </h4>
@@ -81,13 +68,13 @@ function App() {
               </h2>
             </div>
             <div className="card-footer">
-            <a href="#" class="card-link">Card link</a>
+            <a href="#" className="card-link">Card link</a>
             </div>
           </div>
 
           {/* CCI Repo Statuses */}
           {/* Visitor Count  */}
-          <div class="card">
+          <div className="card">
             <h4 className="card-header"> 
               Allconnext Visitor Count
             </h4>
@@ -97,13 +84,13 @@ function App() {
               </h2>
             </div>
             <div className="card-footer">
-            <a href="#" class="card-link">Card link</a>
+            <a href="#" className="card-link">Card link</a>
             </div>
           </div>
 
           {/* Open Multidev Environments  */}
           {/* Visitor Count  */}
-          <div class="card">
+          <div className="card">
             <h4 className="card-header"> 
               Total Weekly Commits
             </h4>
@@ -113,7 +100,7 @@ function App() {
               </h2>
             </div>
             <div className="card-footer">
-            <a href="#" class="card-link">Card link</a>
+            <a href="#" className="card-link">Card link</a>
             </div>
           </div>
         </div>
---
+++ src/Utils/GitHubPullReqs.js
@@ -0,0 +1,23 @@
+const config = require("../config");
+/**
+ * @param {string array} reponame
+ * 
+ * This function returns a count of open pull requests from respective repo names passed in through the reponame string array 
+ */
+
+async function getPullRequestCount(reponame) {
+  try {
+    const prs = await fetch(`${config.RV_PR_ENDPOINT}${reponame}/pulls`, {
+      headers: { 
+        "Authorization": "token " + process.env.REACT_APP_GITHUB_SECRET
+      }
+    });
+    const pullRequests = await prs.json();
+    return pullRequests.length;
+  } catch(e) { 
+    //Error
+    console.log("Hmmm...something went wrong");
+  }
+}
+
+export default getPullRequestCount;
